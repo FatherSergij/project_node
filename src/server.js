13c72c53f3e09565571589024a069152d257a753
root user <roo@localhost>
2022-04-08 08:47:16 +0000

    g

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello World!!77');
+  response.end('Hello World!!7777');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
